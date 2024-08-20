cdfaafa7f00c5ffeecf56816255e9f1ad62599c4
AbiniMA <maabini2002@gmail.com>
2024-08-20 16:06:02 +0530

    added ngif

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
@@ -13,15 +14,37 @@ export class TestComponent {
    {name:'Babyhug Bathtub Shape Soap Dish Kitty Design',price:120},
    ];
 
-   isValid=true;
-
+  //  isValid=true;
+  // isValid:boolean=false;
    carousal=[
     {image:'assets/images/carousel-1.png',title:'Jeep'},
     {image:'assets/images/carousel-2.png ',title:'House'},
     {image:'assets/images/carousel-3.png',title:'Gun'},
     {image:'assets/images/carousel-4.png',title:'Bicycle'},
     {image:'assets/images/carousel-5.png',title:'Truck'},
-   ]
+   ];
+   carousal_doll=[
+    {image:'assets/images/carousel-5.png',title:'Jeep'},
+    {image:'assets/images/carousel-4.png ',title:'House'},
+    {image:'assets/images/carousel-2.png',title:'Gun'},
+    {image:'assets/images/carousel-1.png',title:'Bicycle'},
+    {image:'assets/images/carousel-3.png',title:'Truck'},
+   ];
+//   title='Alo Cart'
+// name='Abini'
+// name(){
+//   return 'Alo Cart'
+// }
+
+// count=0;
+
+// countincrease(){
+//   this.count++
+// }
+
+
+kids=true;
+
 }
 
 
